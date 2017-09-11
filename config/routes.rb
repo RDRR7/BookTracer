b941e313f8112bbb39bd660cf0e617f00c323903
René <rene.d.rendon@unitec.edu>
2017-09-11 11:21:19 -0600

    Added users and homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/home'
+
+  devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations'
+  }
+  
+  root to: "static_pages#home"
 end
